6e80853ccb545e60421c57afd175380aec631773
Christopher Connolly <425070+chaosloth@users.noreply.github.com>
2024-07-04 22:43:52 +1000

    Update to SyncClent 3.x, update project dependencies to later versions

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,16 @@
-import React, { useEffect, useState } from "react";
-import Head from "next/head";
-import type { NextPage } from "next";
-import { Column, Grid } from "@twilio-paste/core";
+import React, { useEffect, useState } from 'react';
+import Head from 'next/head';
+import type { NextPage } from 'next';
+import { Column, Grid } from '@twilio-paste/core';
 
-import { Box } from "@twilio-paste/core/box";
-import { Statistic } from "../components/statistic";
-import { Headline } from "../components/headline";
-import { Footer } from "../components/footer";
+import { Box } from '@twilio-paste/core/box';
+import { Statistic } from '../components/statistic';
+import { Headline } from '../components/headline';
+import { Footer } from '../components/footer';
 
-import SyncClient from "twilio-sync";
-import StatUtil, { Metric, MetricDefinitions } from "../utils/statistics";
-import { LoadingCard } from "../components/loadingCard";
+import { SyncClient } from 'twilio-sync';
+import StatUtil, { Metric, MetricDefinitions } from '../utils/statistics';
+import { LoadingCard } from '../components/loadingCard';
 
 type TaskrouterData = { queues: any[]; workspace: {} };
 
@@ -23,71 +23,71 @@ const Home: NextPage = () => {
   const [token, setToken] = useState<string>();
   const [definitions, setDefinitions] = useState<MetricDefinitions>();
 
-  const BASE_URL = process.env.NEXT_PUBLIC_API_BASE || "";
+  const BASE_URL = process.env.NEXT_PUBLIC_API_BASE || '';
 
   // Helper method to get an access token
   const getToken = () =>
     fetch(`${BASE_URL}/api/token`)
       .then((response) => response.json())
       .then((data) => {
-        setStatus("Got access token");
+        setStatus('Got access token');
         // Create Twilio Sync client with newly received token
         return data.token;
       })
       .catch((reason: any) => {
-        setStatus("Could not fetch token");
-        console.error("Error getting token", reason);
+        setStatus('Could not fetch token');
+        console.error('Error getting token', reason);
       });
 
   // Create sync client and logic for token refresh
   useEffect(() => {
     (async () => {
-      console.log("Fetching access token");
-      setStatus("Fetching access token");
+      console.log('Fetching access token');
+      setStatus('Fetching access token');
 
       try {
         // Get a new token on load
         let token = await getToken();
 
-        console.log("Creating new sync client");
-        setStatus("Creating new sync client");
+        console.log('Creating new sync client');
+        setStatus('Creating new sync client');
 
         let client = new SyncClient(token);
         setClient(client);
 
-        client.on("tokenAboutToExpire", async () => {
-          console.log("tokenAboutToExpire - Updating token for sync client");
-          setStatus("Fetching new access token");
+        client.on('tokenAboutToExpire', async () => {
+          console.log('tokenAboutToExpire - Updating token for sync client');
+          setStatus('Fetching new access token');
           const token = await getToken();
           client.updateToken(token);
           setToken(token);
           setClient(client);
-          setStatus("tokenAboutToExpire - Updated access token");
-          console.log("Updated access token");
+          setStatus('tokenAboutToExpire - Updated access token');
+          console.log('Updated access token');
         });
 
-        client.on("tokenExpired", async () => {
-          console.log("tokenExpired - Updating token for sync client");
-          setStatus("Fetching new access token");
+        client.on('tokenExpired', async () => {
+          console.log('tokenExpired - Updating token for sync client');
+          setStatus('Fetching new access token');
           const token = await getToken();
           client.updateToken(token);
           setToken(token);
           setClient(client);
-          setStatus("Updated expired access token");
-          console.log("tokenExpired - Updated access token");
+          setStatus('Updated expired access token');
+          console.log('tokenExpired - Updated access token');
         });
 
-        client.on("connectionError", async (connectionError) => {
-          console.log("Sync Client Connection error", connectionError);
-          setStatus("Sync Client Connection error - Check logs");
+        client.on('connectionError', async (connectionError) => {
+          console.log('Sync Client Connection error', connectionError);
+          setStatus('Sync Client Connection error - Check logs');
         });
 
-        client.on("connectionStateChanged", async (newState) => {
-          console.log("Sync Connection State", newState);
+        client.on('connectionStateChanged', async (newState) => {
+          console.log('Sync Connection State', newState);
           setStatus(`Sync Client Connection State Changed [${newState}]`);
         });
       } catch (err) {
-        setStatus("Error creating sync client. Check logs");
+        setStatus('Error creating sync client. Check logs');
         console.error(err);
       }
 
@@ -96,8 +96,8 @@ const Home: NextPage = () => {
 
     return () => {
       if (client) {
-        console.log("Shutting down sync client");
-        setStatus("Shutting down sync client");
+        console.log('Shutting down sync client');
+        setStatus('Shutting down sync client');
         client.shutdown();
         setClient(undefined);
       }
@@ -108,17 +108,15 @@ const Home: NextPage = () => {
   useEffect(() => {
     if (client != null) {
       // Create a subscription to the document
-      client
-        .document(process.env.NEXT_PUBLIC_DEFINITIONS_DOCUMENT_NAME)
-        .then((doc) => {
-          console.log("SYNC definitions doc retrieved", doc.value);
-          setDefinitions(doc.value as MetricDefinitions);
-          doc.on("updated", (event) => {
-            console.log("SYNC definitions doc updated", event.value);
-            setDefinitions(event.value);
-            setStatus("Last activity " + new Date().toTimeString());
-          });
+      client.document(process.env.NEXT_PUBLIC_DEFINITIONS_DOCUMENT_NAME).then((doc) => {
+        console.log('SYNC definitions doc retrieved', doc.data);
+        setDefinitions(doc.data as MetricDefinitions);
+        doc.on('updated', (event) => {
+          console.log('SYNC definitions doc updated', event.data);
+          setDefinitions(event.data);
+          setStatus('Last activity ' + new Date().toTimeString());
         });
+      });
     }
   }, [client, token]);
 
@@ -128,34 +126,28 @@ const Home: NextPage = () => {
     if (!client) return;
 
     // Get the statistics document
-    console.log(
-      `Subscribing to sync map: ${process.env.NEXT_PUBLIC_DASHBOARD_SYNC_MAP_SID}`
-    );
+    console.log(`Subscribing to sync map: ${process.env.NEXT_PUBLIC_DASHBOARD_SYNC_MAP_SID}`);
 
     // Update statistic
     const incrementalUpdateData = (item: any) => {
       return setData((data) => {
         if (!data) data = { queues: [], workspace: {} };
-        if (item.key === "WORKSPACE") {
-          data.workspace = item.value;
+        if (item.key === 'WORKSPACE') {
+          data.workspace = item.data;
         } else {
-          const idx = data.queues.findIndex(
-            (q) => q.sid == (item.value as any).sid
-          );
-          idx >= 0
-            ? (data.queues[idx] = item.value)
-            : data.queues.push(item.value);
-          console.log(`Index for ${item.value.friendlyName} is ${idx}`);
+          const idx = data.queues.findIndex((q) => q.sid == (item.data as any).sid);
+          idx >= 0 ? (data.queues[idx] = item.data) : data.queues.push(item.data);
+          console.log(`Index for ${item.key} is ${idx}`);
         }
-        setStatus("Last activity " + new Date().toTimeString());
+        setStatus('Last activity ' + new Date().toTimeString());
         return data;
       });
     };
 
     client.map(process.env.NEXT_PUBLIC_DASHBOARD_SYNC_MAP_SID).then((map) => {
       if (!definitions) {
-        console.warn("Received stats but definitions not set");
-        setStatus("Received stats but definitions not set");
+        console.warn('Received stats but definitions not set');
+        setStatus('Received stats but definitions not set');
         return;
       }
 
@@ -164,25 +156,25 @@ const Home: NextPage = () => {
       });
 
       // Create a subscription to statistics
-      map.on("itemUpdated", (event) => incrementalUpdateData(event.item));
+      map.on('itemUpdated', (event) => incrementalUpdateData(event.item));
     });
   }, [client, token, definitions]);
 
   useEffect(() => {
-    console.log("Data updated", data);
+    console.log('Data updated', data);
   }, [data?.queues, data?.workspace]);
 
   // Update metrics when data changes
   useEffect(() => {
     if (!definitions) {
-      console.warn("Received stat data but definitions not set");
-      setStatus("Received stat data but definitions not set");
+      console.warn('Received stat data but definitions not set');
+      setStatus('Received stat data but definitions not set');
       return;
     }
     let stats = StatUtil.calculate(definitions, data);
-    console.log("Got stats", stats);
+    console.log('Got stats', stats);
     setStatistics(stats);
-    setStatus("Last activity " + new Date().toTimeString());
+    setStatus('Last activity ' + new Date().toTimeString());
     setLoading(false);
   }, [data?.queues, data?.workspace]);
 
@@ -221,11 +213,7 @@ const Home: NextPage = () => {
       <Headline />
 
       <Box as="main" padding="space30">
-        <Grid
-          gutter={["space20", "space60", "space90"]}
-          vertical={[true, true, false]}
-          element="STATGRID"
-        >
+        <Grid gutter={['space20', 'space60', 'space90']} vertical={[true, true, false]} element="STATGRID">
           {loading &&
             loader.map((i) => (
               <Column key={i} span={4} element="STAT">
